47a1a74216c836c821549381a64f8e7a3d741553
josegm124 <eguerrero2118@gmail.com>
2026-05-30 10:23:59 -0600

    submit

--- algorithmic_toolbox/SumTwoDigits.java
+++ algorithmic_toolbox/SumTwoDigits.java
@@ -1,17 +1,15 @@
 package algorithmic_toolbox;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SumTwoDigits {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        BigInteger a = scanner.nextBigInteger();
-        BigInteger b = scanner.nextBigInteger();
-        BigInteger resultado = a.add(b);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int resultado = a + b;
         System.out.println(resultado);
         scanner.close();
     }
-    
 }
